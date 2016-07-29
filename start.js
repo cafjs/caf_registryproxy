6b6241bd249ed6e32f7db245b3a99845c7e2be0c
Antonio Lain <antlai@cafjs.com>
2016-07-29 00:21:30 -0700

    Support eslint

--- start.js
+++ start.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
+'use strict';
 var np = require('./index.js');
 
-np.run([], function(err, top) {
-           if (err) {
-               console.log('Error: ' + err);
-           } else {
-               console.log('Starting registry proxy ...');
-           }
-       });
+np.run([], function(err) {
+    if (err) {
+        // eslint-disable-next-line
+        console.log('Error: ' + err);
+    } else {
+        // eslint-disable-next-line
+        console.log('Starting registry proxy ...');
+    }
+});
